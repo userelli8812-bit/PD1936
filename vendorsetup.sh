056d9b679fad5ba5400518891c9050f38f8e81cf
userelli8812-bit <userelli8812@gmail.com>
2026-07-03 16:53:32 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 export ALLOW_MISSING_DEPENDENCIES=true
-export GIVE_BACK_MISSING_;&euro=true
+export GIVE_BACK_MISSING_accept_pay_back_800;&euro=true
+export ALLOW_PAY_BACK
+export add_account
+export 
+export move 800;euro=true
